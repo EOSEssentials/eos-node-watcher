24290af8894320a314e7c948bd5672e3e843d0d9
leordev <leordev@gmail.com>
2018-07-12 11:36:50 -0400

    basic functions

---
+++ index.js
@@ -0,0 +1,84 @@
+'use strict'
+
+const logger = require('simple-node-logger').createSimpleLogger()
+const nconf = require('nconf')
+const fs = require('fs')
+
+const LAST_BLOCK_FILE = '.lastblock'
+
+let pushers = null
+
+nconf.argv().env('__')
+nconf.defaults({conf: `${__dirname}/config.json`})
+nconf.file(nconf.get('conf'))
+
+const api = require('./lib/eosApi')(nconf.get('eosApi'))
+const actions = require('./lib/actions')(nconf.get('accounts'), nconf.get('receivers'))
+
+const watchInterval = nconf.get('watchInterval')
+
+let lastChainInfo = null
+let nextSyncBlock = nconf.get('initialBlock')
+let isSyncing = true
+
+const mainLoop = async () => {
+
+  if (!pushers)
+    pushers = await require('./lib/pushers')(logger, nconf.get('zeromq'), nconf.get('mongodb'))
+
+  const currentBlock = nextSyncBlock
+  logger.info(`Syncing block ${currentBlock}`)
+
+  try {
+    if (lastChainInfo == null || nextSyncBlock <= lastChainInfo.head_block_num) {
+      lastChainInfo = await api.getChainInfo()
+    }
+
+    if (lastChainInfo && lastChainInfo.head_block_num >= nextSyncBlock) {
+      const blockInfo = await api.getBlockInfo(nextSyncBlock)
+      const transactions = actions.filteredTransactions(blockInfo)
+
+      if (transactions && transactions.length) {
+        pushTransactions(transactions)
+      }
+
+      await saveLastBlock()
+      nextSyncBlock++
+    }
+  } catch (err) {
+    const { message } = err
+    logger.error(`Sync failed on block ${currentBlock} >>> `,
+      message || err || 'Unknown Error')
+  }
+
+  isSyncing = !lastChainInfo || lastChainInfo.head_block_num > nextSyncBlock
+
+  isSyncing ? setImmediate(mainLoop)
+  : setTimeout(mainLoop, watchInterval)
+}
+
+const saveLastBlock = () => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(LAST_BLOCK_FILE, nextSyncBlock, (err) => {
+      if (err)
+        reject(err)
+      else
+        resolve(true)
+    });
+  })
+}
+
+const pushTransactions = transactions => {
+  return Promise.all(pushers.map(p => p(transactions)))
+}
+
+fs.readFile(LAST_BLOCK_FILE, (err,data) => {
+  if (err) {
+    logger.info(`Blocks was never synced, starting from ${nextSyncBlock}`)
+  } else {
+    nextSyncBlock = Number(data) + 1
+    logger.info(`Last synced block: ${nextSyncBlock} - resyncing...`)
+  }
+  mainLoop()
+})
+
